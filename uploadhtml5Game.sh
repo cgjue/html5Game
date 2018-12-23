cbe962e25c965f7a9c60b99b4c46d2cdccd54e84
cgjuee <467784592@qq.com>
2018-12-23 19:41:53 +0800

    update

---
+++ uploadhtml5Game.sh
@@ -0,0 +1,9 @@
+git init
+git config --global user.email "467784592@qq.com"
+git add README
+git add .
+git commit -m "update"
+git remote add origin git@github.com:cgjue/html5Game.git
+#当强制上传到master时使用
+git push -u origin +master
+#git push -u origin master
